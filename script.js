8c72d8b43ced44118af0cf1e81a63c53b2ecf190
Nayra Moura <120141348+NayraMoura@users.noreply.github.com>
2024-11-28 23:08:01 -0300

    Primeiro commit da página inicial

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('enter-button').addEventListener('click', function() {  
+    document.getElementById('welcome-screen').classList.add('hidden');  
+    document.getElementById('homepage').classList.remove('hidden');  
+});
